9f65ec5dfdc80de79d0e03b7e8c35616154f2868
David Davidov <kioltk@gmail.com>
2021-04-26 23:58:52 +0300

    Added headlessui, added dialog

--- resources/js/Pages/Auth/Login.js
+++ resources/js/Pages/Auth/Login.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Helmet from 'react-helmet';
-import { Inertia } from '@inertiajs/inertia';
 import { useForm } from '@inertiajs/inertia-react';
-import Logo from '@/Shared/Logo';
-import LoadingButton from '@/Shared/LoadingButton';
-import TextInput from '@/Shared/TextInput';
+import { Dialog } from '@headlessui/react';
+
 
 export default () => {
   const { data, setData, errors, post, processing } = useForm({
@@ -13,72 +11,34 @@ export default () => {
     remember: true
   });
 
+  let [isOpen, setIsOpen] = useState(true);
+
   function handleSubmit(e) {
     e.preventDefault();
     post(route('login.attempt'));
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen p-6 bg-indigo-900">
-      <Helmet title="Login" />
-      <div className="w-full max-w-md">
-        <Logo
-          className="block w-full max-w-xs mx-auto text-white fill-current"
-          height={50}
-        />
-        <form
-          onSubmit={handleSubmit}
-          className="mt-8 overflow-hidden bg-white rounded-lg shadow-xl"
-        >
-          <div className="px-10 py-12">
-            <h1 className="text-3xl font-bold text-center">Welcome Back!</h1>
-            <div className="w-24 mx-auto mt-6 border-b-2" />
-            <TextInput
-              className="mt-10"
-              label="Email"
-              name="email"
-              type="email"
-              errors={errors.email}
-              value={data.email}
-              onChange={e => setData('email', e.target.value)}
-            />
-            <TextInput
-              className="mt-6"
-              label="Password"
-              name="password"
-              type="password"
-              errors={errors.password}
-              value={data.password}
-              onChange={e => setData('password', e.target.value)}
-            />
-            <label
-              className="flex items-center mt-6 select-none"
-              htmlFor="remember"
-            >
-              <input
-                name="remember"
-                id="remember"
-                className="mr-1"
-                type="checkbox"
-                checked={data.remember}
-                onChange={e => setData('remember', e.target.checked)}
-              />
-              <span className="text-sm">Remember Me</span>
-            </label>
-          </div>
-          <div className="flex items-center justify-between px-10 py-4 bg-gray-100 border-t border-gray-200">
-            <a className="hover:underline" tabIndex="-1" href="#reset-password">
-              Forgot password?
-            </a>
-            <LoadingButton
-              type="submit"
-              loading={processing}
-              className="btn-indigo"
-            >
-              Login
-            </LoadingButton>
-          </div>
-        </form>
+    <div className='flex items-center justify-center min-h-screen p-6 bg-indigo-900'>
+      <Helmet title='Login' />
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+        <Dialog.Overlay />
+
+        <Dialog.Title>Deactivate account</Dialog.Title>
+        <Dialog.Description>
+          This will permanently deactivate your account
+        </Dialog.Description>
+
+        <p>
+          Are you sure you want to deactivate your account? All of your data will
+          be permanently removed. This action cannot be undone.
+        </p>
+
+        <button onClick={() => setIsOpen(false)}>Deactivate</button>
+        <button onClick={() => setIsOpen(false)}>Cancel</button>
+      </Dialog>
+      <div className='w-full max-w-md'>
+        <button>Open Dialog</button>
       </div>
     </div>
   );
